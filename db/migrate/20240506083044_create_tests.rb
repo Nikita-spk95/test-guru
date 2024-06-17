50427c34b6f054c6466e203ce490b2b59858af30
Nikita <y66e0a3307@gmail.com>
2024-06-17 20:25:42 +0300

    5.home_work

--- db/migrate/20240506083044_create_tests.rb
+++ db/migrate/20240506083044_create_tests.rb
@@ -2,10 +2,10 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, default: 1, null: false
+      t.integer :level, null: false, default: 0 
       t.references :category, null: false, foreign_key: true
       t.references :author, null: false, foreign_key: {to_table: :users, on_delete: :cascade}
-      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_20_172129) do
+ActiveRecord::Schema.define(version: 2024_05_06_083750) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -21,27 +21,12 @@ ActiveRecord::Schema.define(version: 2024_05_20_172129) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
-  end
-
-  create_table "results", force: :cascade do |t|
-    t.integer "test_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -53,6 +38,7 @@ ActiveRecord::Schema.define(version: 2024_05_20_172129) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,8 +50,6 @@ ActiveRecord::Schema.define(version: 2024_05_20_172129) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id", on_delete: :cascade
 end
